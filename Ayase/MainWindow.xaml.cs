26c0c2503247ff7c5ad894a02b8c8c0147285471
JeffersonQin <1247006353@qq.com>
2021-08-05 13:06:39 +0800

    chore(Ayase): clear test code

--- Ayase/MainWindow.xaml.cs
+++ Ayase/MainWindow.xaml.cs
@@ -1,26 +1,4 @@
-﻿using Ayase.AutomationLib;
-using Ayase.ThreadLib;
-using Ayase.Wrapper;
-using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Automation;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using UIAutomationClient;
+﻿using System.Windows;
 
 namespace Ayase
 {
@@ -34,9 +12,9 @@ namespace Ayase
             InitializeComponent();
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
     }
 }
