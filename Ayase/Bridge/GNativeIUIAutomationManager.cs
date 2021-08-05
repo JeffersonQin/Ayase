9d4cc82cbfc830554ac89270c9c127142ede669c
JeffersonQin <1247006353@qq.com>
2021-08-05 13:07:35 +0800

    feat(Ayase): Implement P/Invoke briding

---
+++ Ayase/Bridge/GNativeIUIAutomationManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Ayase.Bridge
+{
+    public static class GNativeIUIAutomationManager
+    {
+        [DllImport("../Ayase.Accessibility.dll")]
+        public static extern int InitializeUIAutomation();
+
+        [DllImport("../Ayase.Accessibility.dll")]
+        public static extern int GetLeafElementsFromForegroundWindow(out IntPtr leafElements, out int elementCount);
+
+        [DllImport("../Ayase.Accessibility.dll")]
+        public static extern int DeleteLeafElements(IntPtr leafElements);
+
+        [DllImport("../Ayase.Accessibility.dll")]
+        public static extern int GetGUIElement(IntPtr leafElements, int index, out IntPtr element);
+
+        [DllImport("../Ayase.Accessibility.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetForegroundWindowName();
+
+        [DllImport("../Ayase.Accessibility.dll")]
+        public static extern IntPtr test();
+    }
+}
---
+++ Ayase/Bridge/GNativeUIElement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Ayase.Bridge
+{
+    public static class GNativeUIElement
+    {
+        [DllImport("../Ayase.Accessibility.dll")]
+        public static extern int GetName(IntPtr element, out IntPtr name);
+
+        [DllImport("../Ayase.Accessibility.dll")]
+        public static extern int GetBoundingRectangle(IntPtr element, out double x, out double y, out double w, out double h);
+
+        [DllImport("../Ayase.Accessibility.dll")]
+        public static extern int DeleteUIElement(IntPtr element);
+    }
+}
