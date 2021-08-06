04155bae73fed7a536a63f4e1276dfc0825dc4ad
JeffersonQin <1247006353@qq.com>
2021-08-06 15:41:45 +0800

    feat(Ayase): add hot key library

---
+++ Ayase/HotkeyLib/GHotKey.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Ayase.HotkeyLib
+{
+    public static class GHotKey
+    {
+        #region 常量声明
+        public const int MOD_ALT = 0x0001;
+        public const int MOD_CONTROL = 0x0002;
+        public const int MOD_SHIFT = 0x0004;
+        public const int MOD_WIN = 0x0008;
+        public const int MOD_NOREPEAT = 0x4000;
+        #endregion
+
+        #region Win32API调用
+        [DllImport("user32.dll", EntryPoint = "RegisterHotKey", SetLastError = true)]
+        public static extern int RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        [DllImport("user32.dll", EntryPoint = "UnregisterHotKey", SetLastError = true)]
+        public static extern int UnregisterHotKey(IntPtr hWnd, int id);
+        #endregion
+    }
+}
