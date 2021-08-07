ce2fc5c310775558e8ed0c3f5e8ffd0a90f6baf1
JeffersonQin <1247006353@qq.com>
2021-08-07 10:42:13 +0800

    feat(Ayase): add keybdevent

---
+++ Ayase/HotkeyLib/GKeybdEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Ayase.HotkeyLib
+{
+    public static class GKeybdEvent
+    {
+        public const int KEYEVENTF_EXTENDEDKEY = 1;
+        public const int KEYEVENTF_KEYUP = 2;
+        
+        [DllImport("user32.dll", EntryPoint = "keybd_event", SetLastError = true)]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+    }
+}
