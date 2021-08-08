8f74780d8ed7e05e0dc71a25b91baee2a162a0d3
JeffersonQin <1247006353@qq.com>
2021-08-08 18:06:39 +0800

    feat(Ayase): Add main logic

--- Ayase/App.xaml.cs
+++ Ayase/App.xaml.cs
@@ -1,18 +1,22 @@
 ﻿using Ayase.AccessibilityBridge;
 using Ayase.HotkeyLib;
 using Ayase.ScreenLib;
+using Ayase.ThreadLib;
 using Ayase.UI;
 using Microsoft.Toolkit.Uwp.Notifications;
+using Notifications.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Interop;
+using System.Windows.Threading;
 
 namespace Ayase
 {
@@ -22,6 +26,12 @@ namespace Ayase
     public partial class App : Application
     {
         private SettingsWindow settingsWindow = new SettingsWindow();
+
+        private GThreadPool<ThreadStart> threadPool = new GThreadPool<ThreadStart>(10, new Action<ThreadStart>((threadStart) =>
+        {
+            threadStart.Invoke();
+        }));
+
         private int accessibilityHotKeyId;
 
         protected override void OnStartup(StartupEventArgs e)
@@ -39,7 +49,7 @@ namespace Ayase
             #region show settings window
             settingsWindow.Show();
             #endregion
-
+            
             #region Hotkey registry
             IntPtr hwnd = new WindowInteropHelper(settingsWindow).Handle;
             while (true)
@@ -63,9 +73,20 @@ namespace Ayase
             HwndSource hwndSource = PresentationSource.FromVisual(settingsWindow) as HwndSource;
             hwndSource.AddHook(WndProc);
             #endregion
-
+            
             #region automation registry
-            GNativeIUIAutomationManager.InitializeUIAutomation();
+            new ToastContentBuilder()
+                .AddText("Initializing automation library...")
+                .Show(); 
+            int result = GNativeIUIAutomationManager.InitializeUIAutomation();
+            if (result < 0)
+            {
+                new ToastContentBuilder()
+                    .AddText("Initializing automation library failed")
+                    .AddText("Exiting Program...")
+                    .Show();
+                Shutdown();
+            }
             #endregion
 
             #region initialize window manager
@@ -76,30 +97,109 @@ namespace Ayase
 
         private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handle)
         {
-            if (wParam.ToInt64() > Int32.MaxValue) return IntPtr.Zero;
+            if (wParam.ToInt64() > Int32.MaxValue) 
+                return IntPtr.Zero;
             if (wParam.ToInt32() == accessibilityHotKeyId)
             {
-                WindowManager.CurrentThread = new Thread(() =>
+                #region toggle captial light (offset the negative effect of hotkey)
+                threadPool.EnqueueTask(() =>
                 {
-                    #region toggle captial light (offset the negative effect of hotkey)
                     GKeybdEvent.keybd_event((byte)VirtualKey.VK_MENU, 0x45, 3, 0);
-                    new Thread(() =>
-                    {
-                        Thread.Sleep(200);
-                        GKeybdEvent.keybd_event((byte)VirtualKey.VK_CAPITAL, 0x45,
-                            GKeybdEvent.KEYEVENTF_EXTENDEDKEY | 0, 0);
-                        GKeybdEvent.keybd_event((byte)VirtualKey.VK_CAPITAL, 0x45,
-                            GKeybdEvent.KEYEVENTF_EXTENDEDKEY | GKeybdEvent.KEYEVENTF_KEYUP, 0);
-                    }).Start();
-                    #endregion
-
-                    WindowManager.PresentScreenMask();
+                    Thread.Sleep(200);
+                    GKeybdEvent.keybd_event((byte)VirtualKey.VK_CAPITAL, 0x45,
+                        GKeybdEvent.KEYEVENTF_EXTENDEDKEY | 0, 0);
+                    GKeybdEvent.keybd_event((byte)VirtualKey.VK_CAPITAL, 0x45,
+                        GKeybdEvent.KEYEVENTF_EXTENDEDKEY | GKeybdEvent.KEYEVENTF_KEYUP, 0);
                 });
-                WindowManager.CurrentThread.Start();
+                #endregion
+
+                #region avoid multiple running instance
+                if (WindowManager.ProcessStopFlag != 0)
+                {
+                    WindowManager.notificationManager.Show(
+                        new NotificationContent
+                        {
+                            Title = "Warning",
+                            Message = "Please wait until present process ends.",
+                            Type = NotificationType.Warning
+                        }, areaName: "FormNotificationArea"
+                    );
+                    return IntPtr.Zero;
+                }
+                WindowManager.StartProcess();
+                #endregion
+
+                #region get foreground window
+                GNativeIUIAutomationManager.GetForegroundWindowElement(out IntPtr foregroundWindow);
+                #endregion
+
+                #region present masks
+                GNativeIUIAutomationManager.GetElementBounds(foregroundWindow, out double x, out double y, out double w, out double h);
+
+                WindowManager.PresentScreenMask();
+                WindowManager.PresentFormMask(
+                    x * 96.0 / PrimaryScreen.DpiX,
+                    y * 96.0 / PrimaryScreen.DpiY,
+                    w * 96.0 / PrimaryScreen.DpiX,
+                    h * 96.0 / PrimaryScreen.DpiY);
+                WindowManager.PresentSearchWindow();
+                #endregion
+
+                #region start time costing thread
+                settingsWindow.Dispatcher.Invoke(async () =>
+                {
+                    await Task.Run(() =>
+                    {
+                        #region get elements
+                        DateTime ts_start, ts_end;
+                        ts_start = DateTime.UtcNow;
+                        GNativeIUIAutomationManager.GetLeafElementsFromWindow(foregroundWindow, out IntPtr leafElements, out int elementCount, ref WindowManager.ProcessStopFlag);
+                        ts_end = DateTime.UtcNow;
+
+                        WindowManager.UIElements = leafElements;
+                        WindowManager.UIElementCount = elementCount;
+                        WindowManager.notificationManager.Show(
+                            new NotificationContent
+                            {
+                                Title = "UI Analyzation Completed",
+                                Message = "Time Cost: " + (ts_end - ts_start).TotalSeconds.ToString() + "s\n" + elementCount + " items found",
+                                Type = NotificationType.Success
+                            }, areaName: "FormNotificationArea"
+                        );
+                        
+                        if (WindowManager.ProcessStopFlag > 0)
+                        {
+                            WindowManager.FinishRender();
+                            return;
+                        }
+                        #endregion
+
+                        #region render
+                        for (int i = 0; i < elementCount; i ++)
+                        {
+                            int index = i;
+                            threadPool.EnqueueTask(() =>
+                            {
+                                GNativeIUIAutomationManager.GetGUIElement(leafElements, index, out IntPtr element);
+                                GNativeUIElement.GetBoundingRectangle(element, out double ex, out double ey, out double ew, out double eh);
+                                GNativeUIElement.GetName(element, out IntPtr namePointer);
+                                WindowManager.AddNotationLabel(
+                                    (ex - x) * 96.0 / PrimaryScreen.DpiX,
+                                    (ey - y) * 96.0 / PrimaryScreen.DpiY,
+                                    ew * 96.0 / PrimaryScreen.DpiX,
+                                    eh * 96.0 / PrimaryScreen.DpiY, 
+                                    Marshal.PtrToStringUni(namePointer));
+                            });
+                        }
+                        #endregion
+
+                        WindowManager.FinishRender();
+                    });
+                }, DispatcherPriority.Loaded);
+                #endregion
             }
             return IntPtr.Zero;
         }
-
 
         protected override void OnExit(ExitEventArgs e)
         {
